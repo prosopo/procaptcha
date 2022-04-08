a5b3df5d2297580544a5c1e111c6eec19d2ced79
seht <seht@hyx.net>
2022-04-08 11:12:42 +0100

    tidy

--- packages/ui/src/api/ProsopoContract.ts
+++ packages/ui/src/api/ProsopoContract.ts
@@ -1,9 +1,6 @@
 import ProsopoContractBase from "./ProsopoContractBase";
-import ProviderApi from "./providerApi";
 
-const providerApi = new ProviderApi("http://localhost:3000", "/v1/prosopo");
-
-// TODO: import return types from provider
+// TODO: import return types from provider: separate types/common package.
 class ProsopoContract extends ProsopoContractBase {
     public async getRandomProvider(userAccount?: string): Promise<ProsopoRandomProviderResponse | null> {
         return await this.query('getRandomActiveProvider', [userAccount || this.extension.getAccount().address]) as ProsopoRandomProviderResponse;
