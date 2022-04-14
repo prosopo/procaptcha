e56e8a061d1b08b66a3a179dca8c59504aa1dd52
Chris Taylor <forgetso86@gmail.com>
2022-04-14 12:32:35 +0100

    Move to use @prosopo/contract

--- packages/ui/src/api/ProsopoContract.ts
+++ packages/ui/src/api/ProsopoContract.ts
@@ -1,12 +1,50 @@
-import ProsopoContractBase from "./ProsopoContractBase";
-
+import {ProsopoContractApi, Network} from "@prosopo/contract";
+import Extension, {NoExtensionCallback} from "./Extension";
+import {Abi} from "@polkadot/api-contract";
 
 // TODO: import return types from provider
-class ProsopoContract extends ProsopoContractBase {
+class ProsopoContract extends ProsopoContractApi {
+    public extension: Extension;
+    protected initPromise: Promise<void>;
+
+    constructor(
+        contractAddress: string,
+        abiJson: Record<any, any>,
+        network: Promise<Network>,
+        noExtCb?: NoExtensionCallback
+    ) {
+        const abi = new Abi(abiJson);
+        super(contractAddress, '', abi, network);
+        this.extension = new Extension(noExtCb);
+        this.initPromise = new Promise(async (resolve) => {
+            await this.isReady();
+            this.extension = new Extension(noExtCb);
+            await this.extension.creationPromise();
+            resolve();
+        });
+    }
+
+    /**
+     * await this to make sure creation completes
+     */
+    public async creationPromise() {
+        await this.initPromise;
+        return;
+    }
+
+    public async getExtension() {
+        await this.extension.creationPromise();
+    }
+
     public getRandomProvider() {
         const userAccount = this.extension.getAccount().address;
-        return this.query('getRandomActiveProvider', [userAccount]);
+        return this.contractQuery('getRandomActiveProvider', [userAccount]);
+    }
+
+    public providerRegister(serviceOrigin, fee, payee, address) {
+        return this.contractTx('getRandomActiveProvider', [serviceOrigin, fee, payee, address]);
     }
 }
+
 
 export default ProsopoContract;
--- packages/ui/src/app.tsx
+++ packages/ui/src/app.tsx
@@ -1,180 +1,193 @@
-import React, { useState, useEffect, SyntheticEvent } from "react";
+import React, {useState, useEffect, SyntheticEvent} from "react";
 import "./App.css";
 import ProsopoContract from "./api/ProsopoContract";
 import ProviderApi from "./api/providerApi";
-import { HttpProvider } from "@polkadot/rpc-provider";
 import {
-  Avatar,
-  Box,
-  Button,
-  Typography,
-  Autocomplete,
-  TextField
+    Avatar,
+    Box,
+    Button,
+    Typography,
+    Autocomplete,
+    TextField
 } from "@mui/material";
-import { useStyles } from "./app.styles";
-import { InjectedAccountWithMeta } from "@polkadot/extension-inject/types";
+import {useStyles} from "./app.styles";
+import {InjectedAccountWithMeta} from "@polkadot/extension-inject/types";
 
 import CaptchaPuzzle from "./mockedResponses/captchaPuzzle.json";
+import abiJson from "./abi/prosopo.json";
+import {createNetwork} from "@prosopo/contract";
 
-const contract = new ProsopoContract(
-  new HttpProvider(),
-  "5GVfbq41QmCu4kN9Lx6EGgtzD1MN5YeP4rJRYhKyVBXv8dcw"
-);
+const networkConfig = {'endpoint': 'ws://0.0.0.0:9944'}
+const network = createNetwork('', networkConfig)
+const contract = new ProsopoContract(process.env.CONTRACT_ADDRESS || '', abiJson, network);
 
 const providerApi = new ProviderApi("http://localhost:3000");
 
 declare type UserData = InjectedAccountWithMeta | null | undefined;
 
 function App() {
-  const [account, setAccount] = useState<UserData | null>(null);
-  const classes = useStyles();
-  const [showCaptchas, setShowCaptchas] = useState(false);
-  const [totalNumberOfCaptchas, setTotalNumberOfCaptchas] = useState(0);
-  const [currentCaptchaIndex, setCurrentCaptchaIndex] = useState(0);
+    const [account, setAccount] = useState<UserData | null>(null);
+    const classes = useStyles();
+    const [showCaptchas, setShowCaptchas] = useState(false);
+    const [totalNumberOfCaptchas, setTotalNumberOfCaptchas] = useState(0);
+    const [currentCaptchaIndex, setCurrentCaptchaIndex] = useState(0);
 
-  const accounts = contract.extension?.getAllAcounts();
-  const captchas = CaptchaPuzzle.captchas;
+    const accounts = contract.extension?.getAllAcounts();
+    const captchas = CaptchaPuzzle.captchas;
 
-  useEffect(() => {
-    setTotalNumberOfCaptchas(captchas.length);
-  }, [captchas]);
+    useEffect(() => {
+        setTotalNumberOfCaptchas(captchas.length);
+    }, [captchas]);
 
-  const toggleShowCaptchas = () => {
-    setShowCaptchas(!showCaptchas);
-    setAccount(null);
-  };
+    const toggleShowCaptchas = () => {
+        setShowCaptchas(!showCaptchas);
+        setAccount(null);
+    };
 
-  const cancelCaptchasHandler = () => {
-    setShowCaptchas(false);
-    setAccount(null);
-    setCurrentCaptchaIndex(0);
-  };
+    const registerProvider = () => {
+        contract.providerRegister("https://localhost:8282", 0, "Provider" , account)
+    };
 
-  const submitCaptchaHandler = () => {
-    if (currentCaptchaIndex === totalNumberOfCaptchas - 1) {
-      setShowCaptchas(!showCaptchas);
-      setAccount(null);
-      setCurrentCaptchaIndex(0);
-    } else {
-      setCurrentCaptchaIndex(currentCaptchaIndex + 1);
-    }
-  };
+    const cancelCaptchasHandler = () => {
+        setShowCaptchas(false);
+        setAccount(null);
+        setCurrentCaptchaIndex(0);
+    };
 
-  // useEffect(() => {
-  //   contract
-  //     .creationPromise()
-  //     .then(() => {
-  //       setAccount(contract.extension.getAccount());
-  //     })
-  //     .catch((err) => {
-  //       console.log(err);
-  //     });
-  // }, []);
+    const submitCaptchaHandler = () => {
+        if (currentCaptchaIndex === totalNumberOfCaptchas - 1) {
+            setShowCaptchas(!showCaptchas);
+            setAccount(null);
+            setCurrentCaptchaIndex(0);
+        } else {
+            setCurrentCaptchaIndex(currentCaptchaIndex + 1);
+        }
+    };
 
-  // if (!account) {
-  //   return null;
-  // }
+    // useEffect(() => {
+    //   contract
+    //     .creationPromise()
+    //     .then(() => {
+    //       setAccount(contract.extension.getAccount());
+    //     })
+    //     .catch((err) => {
+    //       console.log(err);
+    //     });
+    // }, []);
 
-  const accountOnChange = (e: SyntheticEvent<Element, Event>, account: any) => {
-    contract.extension.setAccount(account.address).then(async (account) => {
-      setAccount(account);
-      const randomProvider: any = await providerApi.getRandomProvider();
-      console.log(randomProvider);
-      const captchaPuzzle = await providerApi.getCaptchaPuzzle(
-        randomProvider.provider.captcha_dataset_id,
-        randomProvider.provider.service_origin,
-        randomProvider.block_number
-      );
-      console.log(captchaPuzzle);
-    });
-  };
+    // if (!account) {
+    //   return null;
+    // }
 
-  // const onClick = () => {
-  //   const provider = contract.getRandomProvider();
-  // };
+    const accountOnChange = (e: SyntheticEvent<Element, Event>, account: any) => {
+        contract.extension.setAccount(account.address).then(async (account) => {
+            setAccount(account);
+            const randomProvider: any = await providerApi.getRandomProvider();
+            console.log(randomProvider);
+            const captchaPuzzle = await providerApi.getCaptchaPuzzle(
+                randomProvider.provider.captcha_dataset_id,
+                randomProvider.provider.service_origin,
+                randomProvider.block_number
+            );
+            console.log(captchaPuzzle);
+        });
+    };
 
-  return (
-    <Box className={classes.root}>
-      {showCaptchas && !account && (
-        <Autocomplete
-          disablePortal
-          id="select-accounts"
-          options={accounts}
-          value={account}
-          isOptionEqualToValue={(option, value) =>
-            option.address === value.address
-          }
-          onChange={accountOnChange}
-          sx={{ width: 550 }}
-          getOptionLabel={(option: any) =>
-            `${option.meta.name}\n${option.address}`
-          }
-          renderInput={(params) => (
-            <TextField {...params} label="Select account" />
-          )}
-        />
-      )}
+    // const onClick = () => {
+    //   const provider = contract.getRandomProvider();
+    // };
 
-      {showCaptchas && account && (
-        <Box className={classes.captchasContainer}>
-          <Box className={classes.captchasHeader}>
-            <Typography className={classes.captchasHeaderLabel}>
-              Select all images with a bus.
-            </Typography>
-          </Box>
-
-          <Box className={classes.captchasBody}>
-            {Array.from(Array(9).keys()).map((item, index) => {
-              return (
-                <Avatar
-                  key={index}
-                  src="/"
-                  variant="square"
-                  className={classes.captchaItem}
+    return (
+        <Box className={classes.root}>
+            {showCaptchas && !account && (
+                <Autocomplete
+                    disablePortal
+                    id="select-accounts"
+                    options={accounts}
+                    value={account}
+                    isOptionEqualToValue={(option, value) =>
+                        option.address === value.address
+                    }
+                    onChange={accountOnChange}
+                    sx={{width: 550}}
+                    getOptionLabel={(option: any) =>
+                        `${option.meta.name}\n${option.address}`
+                    }
+                    renderInput={(params) => (
+                        <TextField {...params} label="Select account"/>
+                    )}
                 />
-              );
-            })}
+            )}
 
-            <Box className={classes.dotsContainer}>
-              {Array.from(Array(totalNumberOfCaptchas).keys()).map((item) => {
-                return (
-                  <Box
-                    className={classes.dot}
-                    style={{
-                      backgroundColor:
-                        currentCaptchaIndex === item ? "#CFCFCF" : "#FFFFFF"
-                    }}
-                  />
-                );
-              })}
-            </Box>
-          </Box>
+            {showCaptchas && account && (
+                <Box className={classes.captchasContainer}>
+                    <Box className={classes.captchasHeader}>
+                        <Typography className={classes.captchasHeaderLabel}>
+                            Select all images with a bus.
+                        </Typography>
+                    </Box>
 
-          <Box className={classes.captchasFooter}>
-            <Button onClick={cancelCaptchasHandler} variant="text">
-              Cancel
-            </Button>
-            <Button onClick={submitCaptchaHandler} variant="contained">
-              {currentCaptchaIndex === totalNumberOfCaptchas - 1
-                ? "Submit"
-                : "Next"}
-            </Button>
-          </Box>
+                    <Box className={classes.captchasBody}>
+                        {Array.from(Array(9).keys()).map((item, index) => {
+                            return (
+                                <Avatar
+                                    key={index}
+                                    src="/"
+                                    variant="square"
+                                    className={classes.captchaItem}
+                                />
+                            );
+                        })}
+
+                        <Box className={classes.dotsContainer}>
+                            {Array.from(Array(totalNumberOfCaptchas).keys()).map((item) => {
+                                return (
+                                    <Box
+                                        className={classes.dot}
+                                        style={{
+                                            backgroundColor:
+                                                currentCaptchaIndex === item ? "#CFCFCF" : "#FFFFFF"
+                                        }}
+                                    />
+                                );
+                            })}
+                        </Box>
+                    </Box>
+
+                    <Box className={classes.captchasFooter}>
+                        <Button onClick={cancelCaptchasHandler} variant="text">
+                            Cancel
+                        </Button>
+                        <Button onClick={submitCaptchaHandler} variant="contained">
+                            {currentCaptchaIndex === totalNumberOfCaptchas - 1
+                                ? "Submit"
+                                : "Next"}
+                        </Button>
+                    </Box>
+                </Box>
+            )}
+
+            {!showCaptchas && !account && (
+                <div>
+                    <Button
+                        onClick={toggleShowCaptchas}
+                        classes={{root: classes.iAmHumanButton}}
+                    >
+                        <Typography className={classes.iAmHumanButtonLabel}>
+                            I am human
+                        </Typography>
+                    </Button>
+                    <Button
+                        onClick={registerProvider}
+                    >
+                        <Typography>
+                            Register as a Provider
+                        </Typography>
+                    </Button>
+                </div>
+            )}
         </Box>
-      )}
-
-      {!showCaptchas && !account && (
-        <Button
-          onClick={toggleShowCaptchas}
-          classes={{ root: classes.iAmHumanButton }}
-        >
-          <Typography className={classes.iAmHumanButtonLabel}>
-            I am human
-          </Typography>
-        </Button>
-      )}
-    </Box>
-  );
+    );
 }
 
 export default App;
