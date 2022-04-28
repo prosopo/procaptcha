aed5c229419a154f556c2f96d7741006db58d548
seht <seht@hyx.net>
2022-04-28 17:13:42 +0100

    tidy

--- src/config.ts
+++ src/config.ts
@@ -5,8 +5,10 @@
 
 // https://create-react-app.dev/docs/adding-custom-environment-variables/
 
-export default {
+const config: ProviderApiConfig = {
     "providerApi.baseURL": process.env.REACT_APP_API_BASE_URL,
     "providerApi.prefix": process.env.REACT_APP_API_PATH_PREFIX,
     "dappAccount": process.env.REACT_APP_DAPP_CONTRACT_ADDRESS,
 }
+
+export default config;
