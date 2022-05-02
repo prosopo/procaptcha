43fef2416cbeb9de716322ffda8e05c5aa781571
seht <seht@hyx.net>
2022-05-03 00:34:09 +0100

    TransactionResponse type...

--- src/types/contract.d.ts
+++ src/types/contract.d.ts
@@ -1,15 +1,6 @@
 // import { SubmittableResult } from "@polkadot/api";
-
-import type { DispatchError, DispatchInfo, EventRecord, ExtrinsicStatus, Hash } from '@polkadot/types/interfaces';
 import type { SubmittableResultValue } from '@polkadot/api/types';
 
-export declare class TransactionResponse implements SubmittableResultValue {
-    dispatchError?: DispatchError;
-    dispatchInfo?: DispatchInfo;
-    events?: EventRecord[];
-    internalError?: Error;
-    status: ExtrinsicStatus;
-    txHash: Hash;
-    txIndex?: number;
-    blockHash?: string;
-}
+export type TransactionResponse = SubmittableResultValue & { 
+    blockHash?: string,
+};
